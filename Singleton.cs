7f2dc4f34bf677a974c1642bd53d6e5895794067
Harick <117738664+Harick2005@users.noreply.github.com>
2025-04-24 09:40:20 +0700

    Add files via upload

---
+++ Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DigitalOrderSystem.SingletonYeYe;
+
+namespace SingletonYeYe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lấy instance của OrderConfigurationManager
+            var config = OrderConfigurationManager.Instance;
+
+            // Thay đổi giá trị cấu hình
+            config.Currency = "VND";
+
+            // In ra giá trị
+            Console.WriteLine($"Current currency: {OrderConfigurationManager.Instance.Currency}");
+            Console.ReadKey();
+        }
+    }
+}
